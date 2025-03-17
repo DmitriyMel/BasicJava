313ad99e6118d6356059504366b1bbaf55ed927f
DmitriyMel <dmitriymelnikov24@gmail.com>
2025-03-17 16:06:58 +0200

    Added change for merging branch into main

--- src/main/java/ua/org/nmu/dmelnikov/lab_3/Main.java
+++ src/main/java/ua/org/nmu/dmelnikov/lab_3/Main.java
@@ -4,6 +4,6 @@ import ua.org.nmu.dmelnikov.lab_3.controller.TypicalUni;
 
 public class Main {
     public static void main(String[] args) {
-        TypicalUni.createUniversity();
+        TypicalUni.createUniversity();//comment for merging
     }
 }
